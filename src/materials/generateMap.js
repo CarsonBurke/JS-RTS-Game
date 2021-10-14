d32b5a2594482302f72b1937f6a3658c17b95e7f
CarsonBurke <48334001+CarsonBurke@users.noreply.github.com>
2021-10-14 13:25:16 -0700

    added non functional camera, map generation foundations

---
+++ src/materials/generateMap.js
@@ -0,0 +1,17 @@
+function generateMap() {
+
+    createMap()
+    createGrid()
+}
+
+function createMap() {
+
+    map.el.style.width = mapDimensions + "px"
+    map.el.style.height = mapDimensions + "px"
+
+}
+
+function createGrid() {
+
+
+}
---
+++ src/materials/globalVars.js
@@ -0,0 +1,9 @@
+let mapContainer = {
+        el: document.getElementById("mapContainer")
+    },
+    map = {
+        el: document.getElementById("map")
+    },
+    mapDimensions = 500,
+    gridPartDimensions = 20,
+    gridPartSize = mapDimensions / gridPartDimensions
